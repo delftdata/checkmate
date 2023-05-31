50f8f46313ed440ce14083887e209f7cb5eacad2
GianniWiemers <gianniw98@gmail.com>
2023-05-31 15:22:11 +0200

    Add path length check to first map cyclic query

--- n_hop_approximation/operators/first_map.py
+++ n_hop_approximation/operators/first_map.py
@@ -13,9 +13,10 @@ async def find_neighbors(ctx: StatefulFunction, edge: tuple[str, str, int], key_
             current_dict = {}
         if edge[1] in current_dict:
             matching_edges = current_dict[edge[1]]
-        if edge[0] not in current_dict:
-            current_dict[edge[0]] = {}
-        current_dict[edge[0]][edge[1]] = edge[2]
+        if edge[2] == 1:
+            if edge[0] not in current_dict:
+                current_dict[edge[0]] = {}
+            current_dict[edge[0]][edge[1]] = edge[2]
         await ctx.put(current_dict)
     result = []
     result.append(edge)
