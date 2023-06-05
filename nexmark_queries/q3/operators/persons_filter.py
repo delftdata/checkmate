e38a6a09e7e1999b68e92eb06d0e828931f4de2f
George Siachamis <georgedrfsia@gmail.com>
2023-06-05 12:05:05 +0200

    Fix requirements. Change naming convention.

--- nexmark_queries/q3/operators/persons_filter.py
+++ nexmark_queries/q3/operators/persons_filter.py
@@ -3,7 +3,7 @@ from universalis.common.logging import logging
 from universalis.nexmark.entities import Person
 from universalis.common.serialization import Serializer
 
-persons_filter_operator = Operator('persons_filter', n_partitions=6)
+persons_filter_operator = Operator('personsFilter', n_partitions=6)
 
 @persons_filter_operator.register
 async def filter(ctx: StatefulFunction, person: Person):
