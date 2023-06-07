8678409670733f043da81f0eff18efd06350f067
George Siachamis <georgedrfsia@gmail.com>
2023-06-07 11:14:53 +0200

    improve throughput buckets for q1

--- nexmark_queries/q1/metrics/calculate_throughput.py
+++ nexmark_queries/q1/metrics/calculate_throughput.py
@@ -4,27 +4,38 @@ import math
 import matplotlib.pyplot as plt
 
 
+experiment_length = 60 # in seconds
 input_msgs = pd.read_csv('./results/q1/input.csv')
 output_msgs = pd.read_csv('./results/q1/output.csv')
 output_msgs_sorted = output_msgs.sort_values('timestamp')
 
+
 start_time = -math.inf
-throughput = {}
+throughput_buckets = {}
 bucket_id = -1
 
 granularity = 1000  # 1 second (ms) (i.e. bucket size)
 
-for t in output_msgs_sorted['timestamp']:
-    if t - start_time > granularity:
-        bucket_id += 1
-        start_time = t
-        throughput[bucket_id] = 1
+num_of_buckets = int(experiment_length*1000/granularity) + 1
+print(num_of_buckets)
+for i in range(num_of_buckets):
+    throughput_buckets[i] = {}
+    if i == 0:
+        throughput_buckets[i]['bound'] = output_msgs_sorted['timestamp'][0]
     else:
-        throughput[bucket_id] += 1
-print(throughput)
+        throughput_buckets[i]['bound'] = throughput_buckets[i-1]['bound'] + granularity
+    throughput_buckets[i]['items'] = 0
 
+for idx, t in enumerate(output_msgs_sorted['timestamp']):
+    for i in throughput_buckets.keys():
+        if t < throughput_buckets[i]['bound']:
+            throughput_buckets[i]['items'] += 1
+            break
+
+
+throughput_values = [v['items'] for v in throughput_buckets.values()]
 _, ax = plt.subplots()
-ax.plot(throughput.keys(), throughput.values(), linewidth=2.5, label='99p')
+ax.plot(throughput_buckets.keys(), throughput_values, linewidth=2.5, label='output_throughput')
 ax.set_xlabel('Time (s)')
 ax.set_ylabel('Throughput (records/s)')
 ax.legend(bbox_to_anchor=(0.5, -0.2), loc="center", ncol=2)
