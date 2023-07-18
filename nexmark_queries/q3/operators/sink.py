b844ebaec194bc3478d812c750f46e081eef57b7
George Siachamis <georgedrfsia@gmail.com>
2023-07-18 17:35:49 +0300

    fix q8 running

--- nexmark_queries/q3/operators/sink.py
+++ nexmark_queries/q3/operators/sink.py
@@ -1,7 +1,7 @@
 from universalis.common.operator import StatefulFunction, Operator
 from universalis.common.logging import logging
 
-sink_operator = Operator('sink', n_partitions=10)
+sink_operator = Operator('sink', n_partitions=50)
 
 @sink_operator.register
 async def output(ctx: StatefulFunction, joined_tuples):
--- nexmark_queries/q8-running/metrics/calculate_latency.py
+++ nexmark_queries/q8-running/metrics/calculate_latency.py
@@ -1,79 +1,22 @@
 import json
+import sys
 import pandas as pd
 import numpy as np
 import math
 import matplotlib.pyplot as plt
-from universalis.common.networking import NetworkingManager
 
+protocol = sys.argv[1]
 
-networking = NetworkingManager()
-
-input_msgs = pd.read_csv('./results/q3/input.csv').sort_values('timestamp').reset_index()
-output_msgs = pd.read_csv('./results/q3/output.csv').sort_values('timestamp').reset_index()
+input_msgs = pd.read_csv(f'./results/q8r/{protocol}-input.csv')
+output_msgs = pd.read_csv(f'./results/q8r/{protocol}-output.csv')
 experiment_length = 60 # in seconds
 
-# print(input_msgs[['request','timestamp']])
-# print(output_msgs['response'].iloc[0])
+joined = pd.merge(input_msgs, output_msgs, on='request_id', how='outer')
+responded = joined.dropna().sort_values('timestamp_x').reset_index()
+responded = responded[responded['timestamp_x'] > (30000 + responded['timestamp_x'][0])].reset_index()
 
-input_currated = {'request': [], 'timestamp': []}
-for index, row in input_msgs[['request','timestamp']].iterrows():
-    req_dict = eval(row['request'])
-    item_id = req_dict['__MSG__']['__PARAMS__'][0]
-    if len(req_dict['__MSG__']['__PARAMS__']) == 10:
-        input_currated['request'].append(str(item_id)+'_a')
-    else:
-        input_currated['request'].append(str(item_id)+'_p')
-    input_currated['timestamp'].append(row['timestamp'])
-
-input_currated_df = pd.DataFrame.from_dict(input_currated).sort_values('timestamp').reset_index()
-
-
-output_currated = {"response": [], "timestamp_start": [], "timestamp_end": []}
-for index, row in output_msgs[['response','timestamp']].iterrows():
-    resp_list = eval(row['response'])
-    if len(resp_list[0]) == 10:
-        joined_list = [str(resp_list[0][0])+'_a', str(resp_list[1][0])+'_p']
-    else:
-        joined_list = [str(resp_list[0][0])+'_p', str(resp_list[1][0])+'_a']
-    output_currated['response'].append(joined_list)
-    joined_list_df = input_currated_df.loc[input_currated_df['request'].isin(joined_list)].reset_index(drop=True)
-    if joined_list_df.iloc[0,2] >= joined_list_df.iloc[1,2]:
-        output_currated['timestamp_start'].append(joined_list_df.iloc[0,2])
-    else:
-        output_currated['timestamp_start'].append(joined_list_df.iloc[1,2])
-  
-    output_currated['timestamp_end'].append(row['timestamp'])
-
-output_currated_df = pd.DataFrame.from_dict(output_currated).sort_values('timestamp_start').reset_index()
-# print(output_currated_df)
-
-latency_buckets = {}
-bucket_id = -1
-granularity = 100  # 1 second (ms) (i.e. bucket size)
-num_of_buckets = int(experiment_length*1000/granularity) + 1
-print(num_of_buckets)
-for i in range(num_of_buckets):
-    latency_buckets[i] = {}
-    if i == 0:
-        latency_buckets[i]['bound'] = input_currated_df['timestamp'][0]
-    else:
-        latency_buckets[i]['bound'] = latency_buckets[i-1]['bound'] + granularity
-    latency_buckets[i]['items'] = []    
-
-for idx, row in output_currated_df.iterrows():  
-    for i in latency_buckets.keys():
-        if row['timestamp_start'] < latency_buckets[i]['bound']:
-            latency_buckets[i]['items'].append(row['timestamp_end'] - row['timestamp_start'])
-            break
-
-# print(latency_buckets)
-
-# joined = pd.merge(input_msgs, output_msgs, on='request_id', how='outer')
-# responded = joined.dropna().sort_values('timestamp_x').reset_index()
-runtime = []
-for i in latency_buckets.keys():
-    runtime.extend(latency_buckets[i]['items'])
-# print(responded)
+runtime = responded['timestamp_y'] - responded['timestamp_x']
+print(responded)
 
 print(f'min latency: {min(runtime)}ms')
 print(f'max latency: {max(runtime)}ms')
@@ -89,9 +32,32 @@ print(f'10%: {np.percentile(runtime, 10)}ms')
 print(np.argmax(runtime))
 print(np.argmin(runtime))
 
-# # missed = joined[joined['response'].isna()]
-# # print(missed)
+# missed = joined[joined['response'].isna()]
+# print(missed)
 
+
+start_time = -math.inf
+
+latency_buckets = {}
+bucket_id = -1
+granularity = 100  # 1 second (ms) (i.e. bucket size)
+num_of_buckets = int(experiment_length*1000/granularity) + 1
+print(num_of_buckets)
+for i in range(num_of_buckets):
+    latency_buckets[i] = {}
+    if i == 0:
+        latency_buckets[i]['bound'] = responded['timestamp_x'][0]
+    else:
+        latency_buckets[i]['bound'] = latency_buckets[i-1]['bound'] + granularity
+    latency_buckets[i]['items'] = []    
+
+for idx, t in enumerate(responded['timestamp_x']):
+    for i in latency_buckets.keys():
+        if t < latency_buckets[i]['bound']:
+            latency_buckets[i]['items'].append(responded['timestamp_y'][idx] - responded['timestamp_x'][idx])
+            break
+
+# print(latency_buckets)
 
 latency_buckets_99: dict[int, float] = {k*100: np.percentile(v['items'], 99) for k, v in latency_buckets.items() if v['items'] != []}
 latency_buckets_50: dict[int, float] = {k*100: np.percentile(v['items'], 50) for k, v in latency_buckets.items() if v['items'] != []}
@@ -108,6 +74,7 @@ handles, labels = plt.gca().get_legend_handles_labels()
 order = [1,0]
 
 ax.legend([handles[idx] for idx in order],[labels[idx] for idx in order],bbox_to_anchor=(0.5, -0.2), loc="center", ncol=2)
-ax.set_title("NexMark Q8 - No checkpointing")
+ax.set_title(f"NexMark Q8 - {protocol}")
 plt.tight_layout()
-plt.show()
+# plt.show()
+plt.savefig(f'results/q8r/figures/{protocol}')
